57ee9870b17a748fa5a1492f36908ccc41e494a8
Harján Krisztián <erik00000@hotmail.com>
2022-08-31 16:38:38 +0200

    Egy lista összes eleme

---
+++ Gyakorlat_16/main.py
@@ -0,0 +1,6 @@
+# Írjon egy programot,ami kiiratja egy lista összes elemét.
+
+t1= ["Január","Február","Március","Áprlis","Május","Június","Július","Augusztus","Szeptember","Október","November","December"]
+
+for elem in t1:
+    print (elem,"",end="")
