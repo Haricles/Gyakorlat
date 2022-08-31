c7bbc019fc94da5e52cf82e29b112d2302ba5404
Harján Krisztián <erik00000@hotmail.com>
2022-08-31 12:45:27 +0200

    Parallelepipedon terfogata

---
+++ Gyakorlat_5/main.py
@@ -0,0 +1,10 @@
+# Írjon egy programot, ami kiszámolja egy derékszögő parallelepipedon térfogatát,aminek meg van advan a szélessége,
+# a magassága és a hosszúsága.
+
+# Térfogat képlet: V = (a*b)*c
+
+a= 5
+b= 3
+c= 4
+
+print ((a*b)*c)
