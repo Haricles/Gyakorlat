c67291739d590118d8f312ed4e990c379cba5a16
Harján Krisztián <erik00000@hotmail.com>
2022-08-31 13:36:47 +0200

    van benne?

---
+++ Gyakorlat_10/main.py
@@ -0,0 +1,8 @@
+# Írjon egy programot ami meghatározza,hogy egy karakterlánc tartalmazza ez 'e' karaktert.
+
+a = "Volt egyszer egy vadnyugat."
+
+if "e" in a:
+    print ("Van benne.")
+else:
+    print ("Nincs benne.")
