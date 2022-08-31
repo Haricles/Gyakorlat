04bbe68fcbc8ad807213cf2b7b2552df5d81b0b5
Harján Krisztián <erik00000@hotmail.com>
2022-08-31 16:23:56 +0200

    palindrom

---
+++ Gyakorlat_14/main.py
@@ -0,0 +1,11 @@
+# Írjon egy programot amely meghatározza,hogy egy karakterlánc palindrom e. (vagyis mindkét irányból ugyan az.
+# PL: sós,radar
+
+a = "radar"
+if a == a[::-1]:
+    print ("Ez palindrom.")
+else:
+    print ("Ez nem palindrom.")
+
+
+
