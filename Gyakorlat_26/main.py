7b6918c6a9d016504a5214b15c3a0e1653034afb
Harján Krisztián <erik00000@hotmail.com>
2022-09-03 13:52:04 +0200

    n darab ca karakterbol allo string

---
+++ Gyakorlat_26/main.py
@@ -0,0 +1,8 @@
+'''
+Definiáljon egy karakterSor(n,ca) függvényt ami n darab ca karakterből álló stringet ad vissza.
+'''
+
+def karakterSor_02(n,ca):
+    return n*ca
+
+print (karakterSor_02(3,"ca"))
