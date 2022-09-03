ef2104bd911f29d6dbd111daedb18af4c85e263a
Harján Krisztián <erik00000@hotmail.com>
2022-09-03 15:07:03 +0200

    inverse string

---
+++ Gyakorlat_33/main.py
@@ -0,0 +1,9 @@
+'''
+Definiáljon egy inverse(ch) függvényt ami bármilyen stringben megfordítja a karakterek sorrendjét.
+'''
+
+
+def inverse(ch):
+    return ch[::-1]
+
+print (inverse("Programozó leszek"))
