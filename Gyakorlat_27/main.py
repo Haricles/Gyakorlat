52281fea1ee603531d3ee59d728f2807fe172cb0
Harján Krisztián <erik00000@hotmail.com>
2022-09-03 14:00:34 +0200

    kor terület függveny

---
+++ Gyakorlat_27/main.py
@@ -0,0 +1,14 @@
+'''
+Definiáljon egy függyvényt aminek a kör területét kell vissza adni aminek a sugarát argumentumként adjuk meg.
+print korTerület(2.5) utasítás eredményének 19.635-nek kell lenni.
+'''
+
+import  math
+
+def korTerulet(r):
+    return r**2 * math.pi
+
+
+print (korTerulet(2.5))
+
+
