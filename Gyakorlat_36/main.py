90dcf7b420a101fedd7d83c6107914fd4bdb2a71
Harján Krisztián <erik00000@hotmail.com>
2022-09-04 10:47:27 +0200

    egy letezo fajlba addig irok uj sorokat amig entert nem ütök frissitettem a feladat leírásával.

--- Gyakorlat_36/main.py
+++ Gyakorlat_36/main.py
@@ -1,4 +1,10 @@
-
+'''
+Írjon egy scriptet ami lehetővé teszi egy szövegfile kényelmes olvasását. A program előszőr kérje a felhasználótól
+a file nevét. Ezután ajánlja föl a következő választást: új szövegsorokat rögzít vagy ki írja a file tartalmát.
+A felhasználónak be kell tudnia írni az egymást követő szövegsorokat, az <entert> használva a szövegsorok elválasztására
+A beírás befejezéseként elég egy üres sor bevinni (vagyis elég magában leütni az entert). A tartalom kiírásakor
+a soroknak természetes módon kell egymást után következni.
+'''
 with open (input("Kerem a file nevét: "),input("Új szövegsorokat rögzít vagy ki írja a file tartalmát?(a vagy r): "),
            encoding="utf-8") as file:
 
