c67c8c8d8173122cef490a1321d75b66f561ebaa
Harján Krisztián <erik00000@hotmail.com>
2022-09-06 14:10:11 +0200

    páros vagy páratlan szám bekéréssel

---
+++ Gyakorlat_48/main.py
@@ -0,0 +1,11 @@
+'''
+
+Készíts egy programot,ami bekér egy számot a felhasználótól,majd kiírja,hogy a megadott szám páros-e vagy páratlan!
+'''
+
+a = int(input("Kérek egy számot és megmondom,hogy páros vagy páratlan.: "))
+if a % 2 ==0:
+    print ("A számod páros! ")
+else:
+    print ("A számod páratlan. ")
+
