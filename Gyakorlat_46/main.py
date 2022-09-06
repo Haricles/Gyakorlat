562d70b5273ca129008feb0c9dde1b4173fc9937
Harján Krisztián <erik00000@hotmail.com>
2022-09-06 14:00:01 +0200

    jó nap van e megkérdezése

---
+++ Gyakorlat_46/main.py
@@ -0,0 +1,10 @@
+'''
+Készíts egy programot ,amely megkérdezi a felhasználótól,hogy jó napja van-e! A válasz kétféle lehet: igen vagy nem.
+A választól függően írjon ki üzenet a gép!
+'''
+
+a = input("Jó napja van? ")
+if a == "igen":
+    print ("Nagyön örülök ennek.")
+else:
+    print ("Ne búsúljon lesz még jobb is.")
