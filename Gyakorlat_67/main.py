f7b9d11061bfa6ceaf8291b87f9649064b2b853d
Harján Krisztián <erik00000@hotmail.com>
2022-09-07 15:45:44 +0200

    szerepel e a listában

---
+++ Gyakorlat_67/main.py
@@ -0,0 +1,20 @@
+'''
+Alakítsuk át az előbbi programot úgy, hogy a program arról adjon tájékoztatást, hogy pontosan hányszor szerepel a
+felhasználó által megadott szín a listában!
+'''
+
+paletta = ['kék', 'piros', 'fehér', 'fekete', 'zöld', 'sárga', 'barna', 'piros', 'fehér', 'szürke']
+
+bemenet = input("Kerek egy színt! ")
+if bemenet in paletta:
+    print ("Már szerepel benne! ","Eddig ennyiszer szerepel a listában: ",paletta.count(bemenet))
+else:
+    print ("Még nincs benne!")
+
+print ("A lista szinei: ")
+for elem in paletta:
+    print (elem,"",end=",")
+
+
+
+
