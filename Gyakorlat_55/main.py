76cae5b6b55cfc868d117b8063b6059b545c0172
Harján Krisztián <erik00000@hotmail.com>
2022-09-07 13:49:03 +0200

    nev bekeres addig amig entert nem ut a felhasznalo

---
+++ Gyakorlat_55/main.py
@@ -0,0 +1,13 @@
+'''
+Készíts egy programot, amely a felhasználótól keresztneveket kér be egészen addig, amíg az ENTER-t nem üt
+(nem ad meg újabb nevet a bekérésnél)! A program a bekért neveket írja ki a képernyőre!
+
+'''
+
+nevek = input("Kerem a keresztneved: ") # vagy lehetne None is .
+while nevek != "":
+    nevek = input("Kerem a keresztneved: ")
+
+
+
+
