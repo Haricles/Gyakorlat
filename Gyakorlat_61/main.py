b145dd9d5d99edab61243f2d572d92fad80fab96
Harján Krisztián <erik00000@hotmail.com>
2022-09-07 15:05:18 +0200

    kiírja a listaban a t és T betüvel keződő szavakat

---
+++ Gyakorlat_61/main.py
@@ -0,0 +1,15 @@
+'''
+Az adott lista (amely a 'Próbáld ki!' gombra kattintva elérhető) elemei közül a program
+ kiírja a "t" és "T" betűkkel kezdődőeket!
+'''
+
+szavak = ['ajtó','tojás','Ottó','Tamás', 'tép','Tesla','alma','python']
+for elem in szavak:
+    if elem[0] == "t" or elem[0] == "T":
+        print (elem)
+
+
+
+
+
+
