ef4ce94ecf4799b36f3098cf6a5d90af5a04975e
Harján Krisztián <erik00000@hotmail.com>
2022-09-07 15:02:09 +0200

    1-40 intervallumon a 3-al oszthato szamok kiírása

---
+++ Gyakorlat_60/main.py
@@ -0,0 +1,15 @@
+'''
+Készíts egy programot, amely [1; 40] intervallumon kiírja a 3-mal osztható számokat!
+'''
+
+for elem in range(1,41):
+    if elem % 3 ==0:
+        print (elem)
+
+
+
+
+
+
+
+
