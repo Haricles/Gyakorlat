bd28a092c4e050432471814adb7ea7c57e70cb81
Harján Krisztián <erik00000@hotmail.com>
2022-09-07 15:11:08 +0200

    3-al oszthato paros szamok kiirasa egy listabol

---
+++ Gyakorlat_63/main.py
@@ -0,0 +1,15 @@
+'''
+Az adott lista (→ 'Próbáld ki!') elemei közül a program kiírja a 3-mal osztható páros számokat!
+
+'''
+
+szamok = [120, 9, 5, 24, 6, 17, -45, 92, 15, 48, 57]
+for elem in szamok:
+    if elem % 3 == 0 and elem % 2 == 0:
+        print (elem)
+
+
+
+
+
+
