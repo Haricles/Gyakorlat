4d0455cc1d238de4d06392412e364590da52d897
Harján Krisztián <erik00000@hotmail.com>
2022-09-08 15:24:00 +0200

    eljaras definialasa ami eldönti egy szamrol,hogy pozitiv negativ vagy nulla

---
+++ Gyakorlat_90/main.py
@@ -0,0 +1,27 @@
+'''
+Írj eljárást, amely paraméterül kapott számról eldönti, és a képernyőre kiírja, hogy negatív, pozitív vagy nulla-e!
+
+'''
+
+
+def dontes_hozo(szam):
+    if szam < 0:
+        print ("Ez a szám negatív! ")
+    elif szam > 0:
+        print ("Ez a szám pozitív!")
+    else:
+        print ("Ez a szám a 0 !")
+
+dontes_hozo(-1)
+
+
+
+
+
+
+
+
+
+
+
+
