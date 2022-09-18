329bad629fde555345f831bfffd4cba1510b7dea
Harján Krisztián <erik00000@hotmail.com>
2022-09-18 10:52:57 +0200

    Szótár + pprint modul használata + random használata szótárban

--- Gyakorlat_115/main.py
+++ Gyakorlat_115/main.py
@@ -10,25 +10,24 @@ egy-egy szótár egy-egy diák adatait tárolja. A szótárban lévő kulcsok é
 "utca": utcak nevű listából véletlenszerűen választva
 "házszám": véletlenszám [1;50] intervallumban
 A program pprint modul pprint() függvénye segítségével áttekinthető módon jelenítse meg a diákok adatait a képernyőn!
-
 Tipp: listából véletlenszerűen választani a random modul choice() függvényével tudunk a legegyszerűbben.
 '''
 import random
+import pprint
 
 vezeteknevek = ["Kiss", "Horváth", "Szabó", "Pethő", "Szalai", "Nagy"]
 keresztnevek = ["Petra", "Ádám", "Levente", "Zoé", "Hanna"]
 telepulesek = ["Sopron", "Fertőszentmiklós", "Harka", "Kópháza", "Fertőd", "Újkér"]
 utcak = ["Fő", "Kossuth", "Győri", "Petőfi", "Vadvirág", "Iskola"]
 
-vezeteknev= random.choice(vezeteknevek)
-keresztnevek = random.choice(keresztnevek)
-vez_kereszt= (f"{vezeteknev} {keresztnevek}")
+vez_kereszt= random.choice(vezeteknevek)+" "+random.choice(keresztnevek)
+évfoly=random.randint(9,12)
 osztaly = ["A","B","C","D"]
+települes=random.choice(telepulesek)+" "+ random.choice(utcak)+" utca "+ str(random.randint(1,50))
 
 diak_adatok=[{"Név":vez_kereszt,"Kor":random.randint(14,19),
-              "Évfolyam":random.randint(9,12),
-              "Osztaly":random.choice(osztaly)}]
+              "Évfolyam":évfoly,
+              "Osztaly":random.choice(osztaly),
+              "Cím":települes}]
 
-
-
-print (diak_adatok)
+print (pprint.pformat(diak_adatok))
