76e814f2a0c0c96997269457f6804d887af9fdff
Harján Krisztián <erik00000@hotmail.com>
2022-09-28 13:30:09 +0200

    mondatban a szavak száma

---
+++ Gyakorlat_151/main.py
@@ -0,0 +1,43 @@
+'''
+Határozza meg egy adott mondatban a szavak számát
+'''
+
+
+mondat= "Volt egyszer egy vadnyugat."
+
+mondat_2=len((mondat.split()))
+print ("A mondatban a szavak száma:",mondat_2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
