73359eb97796e9e9ef500ed7abf4324413bc5b6a
Harján Krisztián <erik00000@hotmail.com>
2022-09-28 13:22:08 +0200

    2sztringből nevek összeállítása

---
+++ Gyakorlat_150/main.py
@@ -0,0 +1,48 @@
+'''
+Egy amerikai mesében nyolc kiskacsát rendre : Jack, Kack, Lack, Mack, Nack, Oack, Pack és Quack-nak
+hívnak. Írjon egy scriptet, ami ezeket a neveket a következö két stringből állítja elő :
+prefixes = 'JKLMNOPQ' és suffixe = 'ack'
+Ha egy  for ... in ... utasítást alkalmaz, akkor a scriptjének csak két sort kell tartalmazni
+
+'''
+
+prefixes = "JKLMNOPQ"
+suffixe = "ack"
+
+for elem in prefixes:
+    print (elem+suffixe)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
