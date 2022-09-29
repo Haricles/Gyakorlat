bcc780cff1893e0b91a9a40064e12ec5cc391997
Harján Krisztián <erik00000@hotmail.com>
2022-09-29 17:09:32 +0200

    kis es nagybetu függveny majd a ketto egyutt eldönteses modszerrel

---
+++ Gyakorlat_156/main.py
@@ -0,0 +1,82 @@
+'''
+Írjon egy függvényt, aminek akkor « igaz » a visszaérési értéke, ha az argumentuma egy alfabetikus
+karakter (nagy vagy kisbetű). Alkalmazza ebben az új függvényben az előzőekben definiált kisbetu()
+és nagybetu() függvényeket
+
+'''
+
+def nagybetu(x):
+    i=0
+    for elem in x:
+        if elem == elem.capitalize():
+            break
+        else:
+            i+=1
+    if i < len(x):
+        return True
+    else:
+        return False
+
+def kisbetu(x):
+    i = 0
+    for elem in x:
+        if elem == elem.lower():
+            break
+        else:
+            i+=1
+    if i < len(x):
+        return True
+    else:
+        return False
+
+def kis_nagy(x):
+    i=0
+    for elem in x:
+        if (elem == kisbetu(x)) or (elem == nagybetu(x)):
+            break
+        else:
+            i+=1
+    if i < len(x):
+        return True
+    else:
+        return False
+
+def nagybetu_2(x):
+    i=0
+    for elem in x:
+        if (ord(elem) >= 65) and (ord(elem) <=90):
+            break
+        else:
+            i+=1
+    if i < len(x):
+        return True
+    else:
+        return False
+
+def kisbetu_2(x):
+    i = 0
+    for elem in x:
+        if (ord(elem)>=97) and (ord(elem) <=122):
+            break
+        else:
+            i+=1
+    if i < len(x):
+        return True
+    else:
+        return False
+
+def kis_nagybetu_2(x):
+    i=0
+    for elem in x:
+        if nagybetu_2== elem or kisbetu_2 == elem:
+            break
+        else:
+            i+=1
+    if i < len(x):
+        return True
+    else:
+        return False
+
+print (kis_nagybetu_2("24324234"))
+
+
