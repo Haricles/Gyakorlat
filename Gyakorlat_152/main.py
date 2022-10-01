4203dae95611759d6723a60d2fc325b74e9b23b4
Harján Krisztián <erik00000@hotmail.com>
2022-10-01 09:56:51 +0200

    nagybetü függvény eldöntéssel

--- Gyakorlat_152/main.py
+++ Gyakorlat_152/main.py
@@ -5,18 +5,19 @@ nagybetű.
 
 mondat="van Benne nagy betű"
 def nagybetü(x):
+    abc=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
     index=0
-    while index < len(x):
-        if x[index]== x[index].capitalize():
+    for elem in mondat:
+        if elem in abc:
             break
         else:
             index+=1
-    if index < len(x):
+    if index < len(mondat):
         return True
     else:
         return False
 
-print (nagybetü(mondat))
 
+print (nagybetü(mondat))
 
 
